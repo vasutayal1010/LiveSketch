bcedd29f22b63ecb0bf8ce151876740a84f7f2aa
SUMIT DAS <das684704@gmail.com>
2025-01-26 23:44:24 +0530

    error resolved

--- backend/models/boardContent.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-
-const boardContentSchema = new mongoose.Schema({
-    boardId: mongoose.Schema.Types.ObjectId,
-    boardElements: [mongoose.Schema.Types.Mixed]
-})
-
-module.exports = mongoose.model("BoardContent", boardContentSchema);
