a047f8b3222d316f09ec79cfbfff2dd7ed056b79
SUMIT DAS <das684704@gmail.com>
2025-02-10 18:05:45 +0530

    ui board updated

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -140,9 +140,12 @@ const Register = () => {
               Home
             </Link>
             <Link to="/login" 
-                  className="bg-white text-purple-600 px-4 py-2 rounded-lg hover:bg-purple-100 transition duration-300">
-              Login
-            </Link>
+                                className="relative inline-flex items-center justify-center px-6 py-2 overflow-hidden font-medium transition-all bg-white rounded-xl hover:bg-white group">
+                            <span className="w-48 h-48 rounded rotate-[-40deg] bg-purple-900 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+                            <span className="relative w-full text-left text-purple-600 transition-colors duration-300 ease-in-out group-hover:text-white">
+                              Login
+                            </span>
+                          </Link>
           </div>
         </div>
       </header>
