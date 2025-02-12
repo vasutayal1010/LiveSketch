336513279f067320f53bc29acae2b1fbb28d2734
Vasu Tayal <vasutayal1010@gmail.com>
2025-02-12 17:55:22 +0530

    Create BoardItem

--- frontend/src/pages/WhiteBoardsPage.jsx
+++ frontend/src/pages/WhiteBoardsPage.jsx
@@ -1,11 +1,79 @@
-import React from 'react'
-import AppHeader from '../components/CreateNewBoard'
+import React, { useEffect, useState } from "react";
+import { Button, Grid } from "@mui/material";
+import { fetchBoardsForUser } from "../services/apiService";
+import BoardItem from "../components/BoardItem";
+import CreateNewBoard from "../components/CreateNewBoard";
+import AppHeader from "../components/AppHeader";
+
 
 
 const WhiteBoardsPage = () => {
-  return (
-    <AppHeader/>
-  )
-}
+  const userId = localStorage.getItem("userId");
 
-export default WhiteBoardsPage
+  const [isCreateNewBoardPage, setIsCreateNewBoardPage] = useState(false);
+  const [boards, setBoards] = useState([]);
+
+  useEffect(() => {
+    const fetchDataAsync = async () => {
+      try {
+        const responseData = await fetchBoardsForUser(userId);
+        console.log(responseData);
+        setBoards(responseData.respBoard);
+      } catch (error) {
+        // setError(error.message);
+        console.log(`error while loading boads for user : ${error}`);
+      }
+    };
+
+    fetchDataAsync();
+  }, []);
+
+  function handleToggleNewBoard() {
+    setIsCreateNewBoardPage(!isCreateNewBoardPage);
+  }
+
+  return (
+    <div>
+      <AppHeader />
+      <Grid container spacing={2} sx={{ padding: "20px" }}>
+        <Grid item xs={12} md={12}>
+          {!isCreateNewBoardPage && (
+            <Button variant="contained" onClick={handleToggleNewBoard}>
+              Create New Board
+            </Button>
+          )}
+        </Grid>
+
+        {!isCreateNewBoardPage &&
+          boards.map((board) => (
+            <BoardItem
+              key={board._id}
+              boardId={board._id}
+              name={board.boardTitle}
+              description={board.boardDescription}
+              role={board.role}
+              memberCnt={board.members.length}
+              lastAccessedAt={board.lastAccessedAt}
+            />
+          ))}
+
+        {isCreateNewBoardPage && (
+          <CreateNewBoard canBtnHandler={handleToggleNewBoard} />
+        )}
+      </Grid>
+    </div>
+  );
+};
+
+export default WhiteBoardsPage;
+
+// import React from 'react'
+// import AppHeader from '../components/AppHeader'
+
+// function WhiteBoardsPage() {
+//   return (
+//     <AppHeader />
+//   )
+// }
+
+// export default WhiteBoardsPage
